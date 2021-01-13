f4624ee0c249d5f333025dde2f586dfb46610999
bayupercadangan <bayupercadangan@gmail.com>
2021-01-13 07:48:41 +0700

    Store input data mahasiswa to Array

--- tp2_2.c
+++ tp2_2.c
@@ -6,25 +6,32 @@ typedef struct Mahasiswa {
   char *mata_kuliah[20];
 } mahasiswa;
 
-void menu1(mahasiswa *pData) {
-  mahasiswa mhs;
+void menu1(mahasiswa* pData, int* pTotalData) {
+  int total = *pTotalData;
+  mahasiswa mhs[10];
   printf("====== INPUT MAHASISWA ======= \n");
-  printf("Masukkan NIM: "); scanf("%d", &mhs.nim);
-  printf("Masukkan Nama: "); scanf("%255s", mhs.nama);
-  printf("Masukkan Mata Kuliah: "); scanf("%255s", mhs.mata_kuliah);
+  printf("Masukkan NIM: "); scanf("%d", &mhs[total].nim);
+  printf("Masukkan Nama: "); scanf("%255s", mhs[total].nama);
+  printf("Masukkan Mata Kuliah: "); scanf("%255s", mhs[total].mata_kuliah);
 
-  printf("\nNIM : %d", mhs.nim);
-  printf("\nNama: %s", mhs.nama);
-  printf("\nMata Kuliah: %s \n\n\n", mhs.mata_kuliah);
-  *pData = mhs;
+  printf("\nNIM : %d", mhs[total].nim);
+  printf("\nNama: %s", mhs[total].nama);
+  printf("\nMata Kuliah: %s \n\n\n", mhs[total].mata_kuliah);
+  *pData = mhs[10];
+  total++;
+  *pTotalData = total;
 }
 
-void menu2(mahasiswa *pData) {
-  mahasiswa mhs = *pData;
-  printf("===== INPUT NILAI MAHASISWA ====== \n");
-  printf("NIM: %d \n", mhs.nim);
-  printf("NAMA: %s \n", mhs.nama);
-  printf("MATA KULIAH: %s \n", mhs.mata_kuliah);
+void menu2(mahasiswa* pData, int* pTotalData) {
+  int total = *pTotalData;
+  mahasiswa mhs[10];
+  mhs[10] = *pData;
+  for (int i = 0; i < total; i++) {
+    printf("NIM: %d \n", mhs[i].nim);
+    printf("NAMA: %s \n", mhs[i].nama);
+    printf("MATA KULIAH: %s \n", mhs[i].mata_kuliah);
+    printf("\n\n");
+  }
 
   printf("============================\n");
 }
@@ -36,13 +43,14 @@ void menu3() {
 int main () {
   // Variable
   int menu;
+  int totalData = 0;
+  mahasiswa data[10];
 
   // Pointer
   printf("========== PROGRAM MAHASISWA ABAL-ABAL ======== \n");
 
   // Loop program
   do {
-    mahasiswa data;
     printf("Menu Program: \n");
     printf("[1] ====> Input data mahasiswa (NIM, Nama, Mata Kuliah) \n");
     printf("[2] ====> Input nilai mahasiswa \n");
@@ -55,10 +63,10 @@ int main () {
 
     switch (menu) {
       case 1:
-        menu1(&data);
+        menu1(&data[10], &totalData);
         break;
       case 2:
-        menu2(&data);
+        menu2(&data[10], &totalData);
         break;
       case 3:
         menu3();
